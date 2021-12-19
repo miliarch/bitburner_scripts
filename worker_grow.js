880f77e0890dd0acd67eefc7783edd90ee34e6c8
Marcus Bowman <miliarch.mb@gmail.com>
2021-12-19 12:11:59 -0800

    Ditch the dollar sign

--- worker_grow.js
+++ worker_grow.js
@@ -3,7 +3,7 @@ export async function main(ns) {
     // Single operation grower
     var target = ns.args[0];
     var result = await ns.grow(target);
-    var out_str = `${target} grown by factor of \$${ns.nFormat(result, '0.00')}`
+    var out_str = `${target} grown by factor of ${ns.nFormat(result, '0.00')}`
     ns.print(out_str);
     ns.toast(out_str);
 }
