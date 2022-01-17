7e786ef79acdbc99ca22b9f30626a820b1bfea35
Marcus Bowman <miliarch.mb@gmail.com>
2022-01-16 17:08:41 -0800

    Organize singularity lib; implement returnToHome; improve backdoor functions

--- src/singularity/lib.js
+++ src/singularity/lib.js
@@ -6,7 +6,19 @@ export async function main(ns) {
     ns.tprint('use me right, buddy')
 }
 
-export function connectToHost(ns, route, current_host=ns.getServer().hostname) {
+
+// #####################################
+// ### Terminal navigation functions ###
+// #####################################
+
+
+export function buildRouteToHost(ns, hostname, current_host=ns.getCurrentServer()) {
+    var route = []
+    findRouteToHost(ns, '', current_host, hostname, route);
+    return route
+}
+
+export function connectToHost(ns, route, current_host=ns.getCurrentServer()) {
     const operation = 'connect_to_host';
     var result;
     var extra = {};
@@ -24,29 +36,42 @@ export function connectToHost(ns, route, current_host=ns.getServer().hostname) {
     return result
 }
 
-export async function backdoorServer(ns, hostname, tail=false) {
+export function returnToHome(ns) {
+    connectToHost(ns, buildRouteToHost(ns, 'home'));
+}
+
+
+// #################
+// ### Workflows ###
+// #################
+
+export async function backdoorServer(ns, hostname, tail=false, returnHome=true) {
     if (tail) {
         ns.tail();
     }
     const operation = 'backdoor_server';
 
-    // find route to target from current server
-    var current_server = ns.getServer();
-    var route = []
-    findRouteToHost(ns, '', current_server.hostname, hostname, route);
-    connectToHost(ns, route, current_server.hostname);
+    // find route to target
+    connectToHost(ns, buildRouteToHost(ns, hostname));
 
     // install backdoor
     toastPrint(ns, `${operation}: installing backdoor on ${hostname}... (this may take a while)`, 'info', true, true, true);
     await ns.installBackdoor()
 
-    // verify installation and notify user
+    // verify installation
     var target_server = ns.getServer(hostname);
+    var success = target_server.backdoorInstalled;
+
+    // notify user
     var extra = {};
     extra['success'] = `${target_server.hostname}`;
     extra['fail'] = `${target_server.hostname}`;
-    var success = target_server.backdoorInstalled;
     let result = outputMessage(ns, success, true, operation, operation, extra);
+
+    if (returnHome) {
+        // return home if desired
+        returnToHome(ns);
+    }
 
     return result
 }
